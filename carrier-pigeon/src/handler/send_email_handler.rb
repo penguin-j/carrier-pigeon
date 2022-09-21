4cb18d0daf6474aa48d03c9dc5ae19ba3a92a04b
j <penguinjinbei@gmail.com>
2022-09-22 07:45:25 +0900

    feat: mod to_snake_case method

--- carrier-pigeon/src/handler/send_email_handler.rb
+++ carrier-pigeon/src/handler/send_email_handler.rb
@@ -2,15 +2,14 @@ require 'json'
 require './src/service/send_email_service'
 
 def execute(event:, context:)
-  body = to_snake_case(event['body'])
+  body = to_snake_case_keys(event['body'])
   SendEmailService.new(body).execute
 end
 
-private def to_snake_case(body)
-  {
-    'from_address' => body['fromAddress'],
-    'to_address' => body['toAddress'],
-    'subject' => body['subject'],
-    'body' => body['body']
-  }
+private def to_snake_case_keys(hash)
+  hash.transform_keys { |k| to_snake_case(k) }
+end
+
+private def to_snake_case(str)
+  str.gsub(/[A-Z]/) { |s| s = "_#{s.downcase}" }
 end
